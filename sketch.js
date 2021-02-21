9670d0d257dff6d67af2ac2ceddf7abfa658e23e
ElectricManas <74818778+ElectricManas@users.noreply.github.com>
2021-02-21 14:34:59 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+ var dog, happyDog; 
+ var database; 
+ var foodS, foodStock;
+
+function preload()
+{
+	 dog=loadImage("dogimg.png");
+   happyDog = loadImage("dogimg.png"); 
+}
+
+function setup() {
+	createCanvas(500,500);
+  createSprite
+  
+}
+
+
+function draw() {  
+  background(46, 139, 87); 
+  drawSprites();
+  //add styles here
+
+}
+
+
+
